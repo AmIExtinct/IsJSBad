a9a4a52c05b1a62e2ec5cbf3af5e9ec783eae33a
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-03-27 09:26:24 -0500

    Now You Wont

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var but = document.getElementById('themeButton')
+
+
+function switchTheme(){
+   bat = document.createElement('p')
+   bat.innerText = "Where did I come from?"
+   document.getElementsByName('html').append(bat)
+   
+   
+}
+but.addEventListener("click", switchTheme)
+
+
