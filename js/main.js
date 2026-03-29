ae327381b538a0358e8d741e2cba75658b7ca508
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-03-29 13:17:56 -0500

    will it add?

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+var body = document.body
 var but = document.getElementById('themeButton')
 
 
 function switchTheme(){
    bat = document.createElement('p')
    bat.innerText = "Where did I come from?"
-   document.getElementsByName('html').append(bat)
    
+  body.appendChild(bat) 
    
 }
 but.addEventListener("click", switchTheme)
