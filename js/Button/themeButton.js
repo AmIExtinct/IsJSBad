e7c8ce32c9510f1c1dace59562069affaefc02e1
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-04-11 13:52:52 +0000

    Yep Broke it

--- js/Button/themeButton.js
+++ js/Button/themeButton.js
@@ -16,4 +16,31 @@ function changeTheme(){
    isDark = true
 }
 
+function browserSpeak(msg, rate=1.2){
+   const bruh = new SpeechSynthesisUtterance(msg)
+   bruh.rate = rate
+   bruh.volume = 1
+   window.speechSynthesis.speak(bruh)
+}
+
+function onMouseEnter(){
+   browserSpeak("what Are You Trying To do?")
+
+   if (document.getElementById('catchButton') != null){
+      let catchBut = document.getElementById('catchButton')
+      catchBut.style.display = 'block'
+   }
+   let catchButton = document.createElement('button')
+   catchButton.id = "catchButton"
+   catchButton.classList.add('base-Style')
+   document.body.appendChild(catchButton)
+}
+
+function onMouseLeave(){
+   let catchButton = document.getElementById('catchButton')
+   catchButton.style.display = 'none'
+}
+
 themeBut.addEventListener('click', changeTheme)
+themeBut.addEventListener('mouseenter', onMouseEnter)
+themeBut.addEventListener('mouseleave', onMouseLeave)
